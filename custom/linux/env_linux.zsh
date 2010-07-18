6cc486df2f9f3ec2d2c91eee0ffcbe549ec69406
lokijuhz <mail@petervasil.net>
2010-07-18 15:19:27 +0200

    Added GIT_EXTERNAL_DIFF variable.

--- custom/linux/env_linux.zsh
+++ custom/linux/env_linux.zsh
@@ -31,3 +31,6 @@ export PATH=$PATH:$JRUBY_HOME/bin
 export PYTHONPATH=/usr/lib/python2.6/site-packages:${PYTHONPATH}
 
 export PATH=$PATH:~/src/fast-export/bin
+
+#export GIT_EXTERNAL_DIFF=/home/peter/bin/extDiff
+export GIT_EXTERNAL_DIFF=/home/peter/bin/extP4diff
