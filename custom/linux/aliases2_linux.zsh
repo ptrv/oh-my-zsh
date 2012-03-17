0b4dab7b1dba9274773764ac620391b1a3b39a0e
ptrv <mail@petervasil.net>
2012-03-17 12:30:49 +0100

    Add shortcut aliases.

---
+++ custom/linux/aliases2_linux.zsh
@@ -0,0 +1,24 @@
+export MYTEMP=~/Documents/Temp
+export MYCODECPP=~/development
+export MYCODERUBY=~/development_ruby
+export MYCODEPYTHON=~/development_python
+export MYCODEJAVA=~/development_java
+export MYDOWNLOADS=~/Downloads
+export MYDOCUMENTS=~/Documents
+export MYUNI=~/tu
+export MYAPPS=~/applications
+
+# commands to change dirs
+alias to.temp='cd ${MYTEMP}'
+alias to.downloads='cd ${MYDOWNLOADS}'
+alias to.docs='cd ${MYDOCUMENTS}'
+alias to.cpp='cd ${MYCODECPP}'
+alias to.ruby='cd ${MYCODERUBY}'
+alias to.python='cd ${MYCODEPYTHON}'
+alias to.java='cd ${MYCODEJAVA}'
+alias to.uni='cd ${MYUNI}'
+alias to.apps='cd ${MYAPPS}'
+
+#OFten useful to prefix with SUDO to see more system level network usage
+alias network.connections='sudo lsof -l -i +L -R -V'
+alias network.established='sudo lsof -l -i +L -R -V | grep ESTABLISHED'
