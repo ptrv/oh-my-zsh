9e988046f0e2456d00428846407d8939777ed46a
ptrv <mail@petervasil.net>
2012-07-03 23:06:50 +0200

    move function custom/function.

--- custom/misc.zsh
+++ custom/misc.zsh
@@ -1,4 +1,5 @@
-autoload -U $ZSH/functions/*(:t)
+fpath=($fpath $ZSH/custom/functions)
+autoload -U $ZSH/custom/functions/*(:t)
 
 REPORTTIME=10 # Show elapsed time if command took more than X seconds
 
