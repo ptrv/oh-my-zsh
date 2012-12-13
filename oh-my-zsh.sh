cd20fc9a4952c0c841548d36f2ff8c0b054c430b
ptrv <mail@petervasil.net>
2012-12-13 12:43:47 +0100

    Remove duplicate code from oh-my-zsh.sh

--- oh-my-zsh.sh
+++ oh-my-zsh.sh
@@ -9,9 +9,6 @@ fi
 # add a function path
 fpath=($ZSH/functions $ZSH/completions $fpath)
 
-# Load all of your custom configurations from custom/
-for config_file ($ZSH/custom/*.zsh) source $config_file
-
 # Load all of the config files in ~/oh-my-zsh that end in .zsh
 # TIP: Add files you don't want in git to .gitignore
 for config_file ($ZSH/lib/*.zsh); do
