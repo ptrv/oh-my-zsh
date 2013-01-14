3c568d4816e8b19fb3df08b584e09e24efdaa221
ptrv <mail@petervasil.net>
2013-01-14 14:54:17 +0100

    Change tab completion

--- custom/ptrv_custom.zsh
+++ custom/ptrv_custom.zsh
@@ -10,7 +10,8 @@ export LS_COLORS='di=1;34:ln=35:so=32:pi=1;33:ex=32:bd=34;46:cd=34;43:su=0;41:sg
 ################################################
 
 zstyle ':completion:*:default' list-colors ${(s.:.)LS_COLORS}
-zstyle ':completion:*:*:*:*:*' menu yes select
+#zstyle ':completion:*:*:*:*:*' menu yes select
+zstyle ':completion:*:*:*:*:*' select
 
 if [ -s "$ZSH_CUSTOM/pandoc-completion/pandoc-completion.bash" ]
 then
